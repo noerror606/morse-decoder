e68a05c0cdf596597f671d21c5c760d94fc1f6cd
noerror606 <55393548+noerror606@users.noreply.github.com>
2023-08-08 23:19:40 +0300

    feat: add solution task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,24 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    const letters = expr.match(/.{1,10}/g); // разбиваем строку на закодированные буквы
+    console.log(letters)
+    let mors = []
+    let result = ""
+    for(let i = 0; i<letters.length; i++){
+        mors.push(letters[i].replace(/10/g, ".").replace(/11/g, "-").replace(/[*]{10}/g, " ").replace(/0/g, ""));// веселая конструкция где мы меняем все 10 и 11 на символы а последний удаляет лишние нули и все это записываем в новый массив 
+ 
+    }
+   for (let i = 0; i < mors.length; i++) {
+    if (mors[i] === " ") {
+      result += " "; // добавляем пробел в результат
+    } else {
+      result += MORSE_TABLE[mors[i]]; // получаем букву из таблицы Морзе и добавляем в результат
+    }
+  }
+
+  
+    return(result);
 }
 
 module.exports = {
